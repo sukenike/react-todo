35109cd347867f49078e7645ecd7adcf105f2a43
sukenike <suke38@gmail.com>
2021-05-25 18:41:50 +0900

    入力の取得

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState([
     "あああああ",
     "いいいいい"
   ]);
   const [completeTodos, setCompleteTodos] = useState(["ううううう"]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
         <button>追加</button>
       </div>
       <div className="incomplete-area">
