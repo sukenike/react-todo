b4f3bc8e4cb260bae235948a6111a2edb5ee85bb
sukenike <suke38@gmail.com>
2021-05-25 19:46:29 +0900

    登録するTODOの上限設定

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,12 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>５コまでです。</p>
+      )}
+
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
